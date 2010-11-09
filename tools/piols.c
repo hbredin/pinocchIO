ad0a20a1cc62100d1297e526737611abbcff2354
Herve Bredin <bredin@limsi.fr>
2010-11-09 17:38:42 +0100

    piols: new options to display dataset/timeline details

--- tools/piols.c
+++ tools/piols.c
@@ -34,7 +34,11 @@ void usage(const char * path2tool)
 			"       --no-timeline\n"
 			"           Do not list timelines\n"
 			"       --no-dataset\n"
-			"           Do not list datasets\n");
+			"           Do not list datasets\n"
+            "   -t, --timeline=PATH\n"
+            "           Show more detail about timeline at PATH\n"
+            "   -d, --dataset=PATH\n"
+            "           Show more detail about dataset at PATH\n");
 	fflush(stdout);
 }
 
@@ -64,7 +68,9 @@ void pretty_print(const char * path2dataset, const char * description)
 int main (int argc, char *const  argv[])
 {	
 	char* pinocchio_file = NULL;
-	
+    char* dataset = NULL;
+    char* timeline = NULL;
+    
 	int c;
 	while (1)
 	{
@@ -77,12 +83,14 @@ int main (int argc, char *const  argv[])
 			 We distinguish them by their indices. */
 			{"no-timeline", no_argument, &timeline_flag, 0},
 			{"no-dataset",  no_argument, &dataset_flag,  0},
+            {"timeline",    required_argument, 0, 't'},
+            {"dataset",     required_argument, 0, 'd'},
 			{0, 0, 0, 0}
 		};
 		/* getopt_long stores the option index here. */
 		int option_index = 0;
 		
-		c = getopt_long (argc, argv, "h",
+		c = getopt_long (argc, argv, "hd:t:",
 						 long_options, &option_index);
 		
 		/* Detect the end of the options. */
@@ -105,7 +113,15 @@ int main (int argc, char *const  argv[])
 				usage(argv[0]);
 				exit(-1);
 				break;
-				
+
+            case 't':
+                timeline = optarg;
+				break;
+                
+            case 'd':
+                dataset = optarg;
+				break;
+                
 			case '?':
 				/* getopt_long already printed an error message. */
 				usage(argv[0]);
@@ -135,6 +151,98 @@ int main (int argc, char *const  argv[])
 		exit(-1);
 	}
 	
+    if (dataset)
+    {
+        PIODataset pioDataset = pioOpenDataset(PIOMakeObject(pioFile), dataset);
+        if (PIODatasetIsInvalid(pioDataset))
+        {
+            fprintf(stderr, "Cannot open dataset %s.\n", dataset);
+            fflush(stderr);
+            pioCloseFile(&pioFile);
+            exit(-1);
+        }
+        
+        fprintf(stdout, "Path        = %s\n", pioDataset.path);
+        fprintf(stdout, "Description = %s\n", pioDataset.description);
+        fflush(stdout);
+        
+        PIOTimeline pioTimeline = pioGetTimeline(pioDataset);
+        if (PIOTimelineIsInvalid(pioTimeline))
+        {
+            fprintf(stderr, "Cannot open timeline of dataset %s.\n", dataset);
+            fflush(stderr);
+            pioCloseDataset(&pioDataset);
+            pioCloseFile(&pioFile);
+            exit(-1);
+        }
+        fprintf(stdout, "Timeline\n");
+        fprintf(stdout, "   Path        = %s\n", pioTimeline.path);
+        fprintf(stdout, "   Description = %s\n", pioTimeline.description);
+        fprintf(stdout, "   Length      = %d\n", pioTimeline.ntimeranges);
+        fflush(stdout);
+        pioCloseTimeline(&pioTimeline);
+        
+        PIODatatype pioDatatype = pioGetDatatype(pioDataset);
+        if (PIODatatypeIsInvalid(pioDatatype))
+        {
+            fprintf(stderr, "Cannot open datatype of dataset %s.\n", dataset);
+            fflush(stderr);
+            pioCloseDataset(&pioDataset);
+            pioCloseFile(&pioFile);
+            exit(-1);            
+        }
+        
+        fprintf(stdout, "Datatype\n");
+        fprintf(stdout, "   Dimension = %d\n", pioDatatype.dimension);
+        switch (pioDatatype.type) 
+        {
+            case PINOCCHIO_TYPE_CHAR:
+                fprintf(stdout, "   Base type = CHAR\n");                
+                break;
+            case PINOCCHIO_TYPE_INT:
+                fprintf(stdout, "   Base type = INT\n");                
+                break;
+            case PINOCCHIO_TYPE_FLOAT:
+                fprintf(stdout, "   Base type = FLOAT\n");                
+                break;
+            case PINOCCHIO_TYPE_DOUBLE:
+                fprintf(stdout, "   Base type = DOUBLE\n");                
+                break;
+            default:
+                fprintf(stdout, "   Base type = UNKNOWN\n");                
+                break;
+        }
+        fflush(stdout);
+        pioCloseDatatype(&pioDatatype);
+        
+        pioCloseDataset(&pioDataset);
+        pioCloseFile(&pioFile);
+        exit(-1);
+    }
+
+    
+    if (timeline)
+    {
+        PIOTimeline pioTimeline = pioOpenTimeline(PIOMakeObject(pioFile), timeline);
+        if (PIOTimelineIsInvalid(pioTimeline))
+        {
+            fprintf(stderr, "Cannot open timeline %s.\n", timeline);
+            fflush(stderr);
+            pioCloseFile(&pioFile);
+            exit(-1);
+        }
+        
+        fprintf(stdout, "Path        = %s\n", pioTimeline.path);
+        fprintf(stdout, "Description = %s\n", pioTimeline.description);
+        fprintf(stdout, "Length      = %d\n", pioTimeline.ntimeranges);
+        fflush(stdout);
+
+        pioCloseTimeline(&pioTimeline);        
+        pioCloseFile(&pioFile);
+        exit(-1);
+    }
+    
+    
 	fprintf(stdout, "== Medium ==\n");
 	fprintf(stdout, "%s\n", pioFile.medium);
 	fflush(stdout);
