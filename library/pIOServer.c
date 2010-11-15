1985cf0b26df552fee3b8bbfece6ff21ec6c2c7f
Herve Bredin <bredin@limsi.fr>
2010-11-15 15:31:13 +0100

    pIOServer: new pioNewServerFromConfigurationFile and pioDumpServer

--- library/pIOServer.c
+++ library/pIOServer.c
@@ -17,7 +17,82 @@
 #include "pIOTimeline.h"
 #include "pIORead.h"
 
-PIOServer pioNewServer(const char** files, int nfiles, 
+PIOServer pioNewServerFromConfigurationFile(const char* filename)
+{
+    config_t config;
+    const config_setting_t *config_setting = NULL;
+    
+    char** files = NULL;
+    int nfiles = 0;
+    int f, ff;
+    const char* cur_file = NULL;
+    const char* cur_dataset = NULL;
+    char* dataset = NULL;
+    
+    PIOServer server = PIOServerInvalid;
+    config_init(&config);
+    config_read_file(&config, filename);
+    if (config_lookup_string(&config, "dataset", &cur_dataset) == CONFIG_FALSE)
+    {
+        fprintf(stderr, "Could not find path to dataset in configuration file %s.\n",
+                filename);
+        config_destroy(&config);
+        return PIOServerInvalid;        
+    }
+    
+    dataset = (char*)malloc((strlen(cur_dataset)+1)*sizeof(char));
+    sprintf(dataset, "%s", cur_dataset);
+    
+    config_setting = config_lookup(&config, "files");
+    if (config_setting)
+    {
+        if (config_setting_is_list(config_setting) != CONFIG_TRUE)
+        {
+            fprintf(stderr, "Files must be provided as a list of files.\n");
+            fprintf(stderr, "Example: files = ( \"file1\", \"file2\" );");
+            config_destroy(&config);
+            return PIOServerInvalid;            
+        }
+        
+        nfiles = config_setting_length(config_setting);
+        files = (char**) malloc(nfiles * sizeof(char*));
+        for (f=0; f<nfiles; f++)
+        {
+            cur_file = config_setting_get_string_elem(config_setting, f);
+            
+            if (!cur_file)
+            {
+                fprintf(stderr, 
+                        "There must be something wrong with %dth file in list.\n", 
+                        f+1);
+                for (ff=0; ff<f; ff++) free(files[ff]); free(files);
+                config_destroy(&config);
+                return PIOServerInvalid;                            
+            }
+
+            files[f] = (char*) malloc((strlen(cur_file)+1)*sizeof(char));
+            sprintf(files[f], "%s", cur_file);
+        }
+    }
+    else
+    {
+        fprintf(stderr, "Could not find list of files in configuration file %s.\n",
+                filename);
+        config_destroy(&config);
+        return PIOServerInvalid;
+    }
+
+    config_destroy(&config);
+    
+    server = pioNewServer(files, nfiles, dataset);
+
+    for (f=0; f<nfiles; f++) free(files[f]); free(files);
+    free(dataset);
+    
+    return server;
+}
+
+PIOServer pioNewServer(char** files, int nfiles, 
                        const char* path)
 {
     int f = 0;
@@ -141,7 +216,12 @@ PIOServer pioNewServer(const char** files, int nfiles,
     return pioServer;
 }
 
-int pioReadNext(PIOServer* server, PIODatatype pioDatatype, void** buffer)
+int pioGetServerDimension(PIOServer server)
+{
+    return server.datatype.dimension;
+}
+
+int pioReadNextData(PIOServer* server, PIODatatype pioDatatype, void** buffer)
 {
     int number = -1;
     
@@ -177,6 +257,74 @@ int pioReadNext(PIOServer* server, PIODatatype pioDatatype, void** buffer)
     
     return number;
 }
+
+int pioDumpServer(PIOServer* pioServer, 
+                  PIODatatype pioDatatype, 
+                  void* already_allocated_buffer)
+{
+    int ds = 0;
+    
+    int tmp_number = -1;
+    int number = 0;
+        
+    size_t tmp_size = -1;
+    size_t size = 0;
+    
+    if (already_allocated_buffer)
+    {
+        for (ds=0; ds<pioServer->nfiles; ds++) 
+        {
+            pioServer->current_file = pioOpenFile(pioServer->files[ds], 
+                                                  PINOCCHIO_READONLY);
+            pioServer->current_dataset = pioOpenDataset(PIOMakeObject(pioServer->current_file), 
+                                                        pioServer->dataset);
+            
+            // dump dataset at correct position
+            tmp_number = pioDumpDataset(&(pioServer->current_dataset), 
+                                        pioDatatype, 
+                                        already_allocated_buffer+size);
+            
+            // close what needs to be closed
+            pioCloseDataset(&(pioServer->current_dataset));
+            pioCloseFile(&(pioServer->current_file));
+            
+            if (tmp_number < 0) return -1; // stop if something bad happened
+            
+            tmp_size = tmp_number * H5Tget_size(pioDatatype.identifier);
+            size = size + tmp_size;
+            
+            number = number + tmp_number;
+        }    
+    }
+    else 
+    {
+        for (ds=0; ds<pioServer->nfiles; ds++) 
+        {
+            pioServer->current_file = pioOpenFile(pioServer->files[ds], 
+                                                  PINOCCHIO_READONLY);
+            pioServer->current_dataset = pioOpenDataset(PIOMakeObject(pioServer->current_file), 
+                                                        pioServer->dataset);
+            
+            // dump dataset at correct position
+            tmp_size = pioDumpDataset(&(pioServer->current_dataset), 
+                                        pioDatatype, 
+                                        NULL);
+            
+            // close what needs to be closed
+            pioCloseDataset(&(pioServer->current_dataset));
+            pioCloseFile(&(pioServer->current_file));
+            
+            if (tmp_size < 0) return -1; // stop if something bad happened
+            
+            size = size + tmp_size;            
+        }        
+        
+        return size;
+    }
+    
+    return number;
+}
+
 
 int pioCloseServer(PIOServer* pioServer)
 {
--- library/pIOServer.h
+++ library/pIOServer.h
@@ -10,14 +10,31 @@
 #ifndef _PINOCCHIO_SERVER_H
 #define _PINOCCHIO_SERVER_H
 
+#include <libconfig.h>
+
 #include "pIOTypes.h"
 
+#define PIOServerIsInvalid(o)   ((o).nfiles < 1)
+#define PIOServerIsValid(o)     (!PIOServerIsInvalid(o))
 
-PIOServer pioNewServer(const char** files, int nfiles, 
+PIOServer pioNewServer(char** files, int nfiles, 
                        const char* path);
-
-int pioReadNext(PIOServer* server, PIODatatype pioDatatype, void** buffer);
+PIOServer pioNewServerFromConfigurationFile(const char* path);
 
 int pioCloseServer( PIOServer* server );
+
+int pioGetServerDimension(PIOServer server);
+
+int pioReadNextData(PIOServer* server,
+                    PIODatatype pioDatatype,
+                    void** buffer);
+
+// 2 steps usage:
+//   - already_allocated_buffer = malloc(pioDumpServer(pioServer, pioDatatype, NULL))
+//   - number = pioDumpServer(pioServer, pioDatatype, already_allocated_buffer)
+int pioDumpServer(PIOServer* server, 
+                  PIODatatype pioDatatype, 
+                  void* already_allocated_buffer);
+
 
 #endif
