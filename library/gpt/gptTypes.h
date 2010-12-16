624006390c95978b1ebb314a7cec5c343376e88b
Herve Bredin <bredin@limsi.fr>
2010-12-16 11:20:34 +0100

    pinocchIO is now under GNU GPL v3 license

--- library/gpt/gptTypes.h
+++ library/gpt/gptTypes.h
@@ -1,11 +1,22 @@
-/*
- *  gptTypes.h
- *  pinocchIO
- *
- *  Created by Hervé BREDIN on 18/11/10.
- *  Copyright 2010 __MyCompanyName__. All rights reserved.
- *
- */
+// 
+// Copyright 2010 Herve BREDIN (bredin@limsi.fr)
+// Contact: http://pinocchio.niderb.fr/
+// 
+// This file is part of pinocchIO.
+//  
+//      pinocchIO is free software: you can redistribute it and/or modify
+//      it under the terms of the GNU General Public License as published by
+//      the Free Software Foundation, either version 3 of the License, or
+//      (at your option) any later version.
+//  
+//      pinocchIO is distributed in the hope that it will be useful,
+//      but WITHOUT ANY WARRANTY; without even the implied warranty of
+//      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//      GNU General Public License for more details.
+//  
+//      You should have received a copy of the GNU General Public License
+//      along with pinocchIO. If not, see <http://www.gnu.org/licenses/>.
+// 
 
 #ifndef _GEPETTO_TYPES_H
 #define _GEPETTO_TYPES_H
