4db9112b232ce1a8c4f229893dd6c7d8e01d5943
Herve Bredin <bredin@limsi.fr>
2011-01-17 11:40:04 +0100

    pioWrite bug fix: now works as expected when called with no data (i.e. number = 0)

--- library/pio/pIOWrite.c
+++ library/pio/pIOWrite.c
@@ -42,28 +42,32 @@ int pioWrite(PIODataset* pioDataset, int timerangeIndex,
 	hid_t dataspaceForLink = -1;
 	hid_t bufferDataspaceForLink = -1;
 	hid_t link_datatype = -1;
-	
+
+    if (dataNumber < 0) return -1;
 	if (!(timerangeIndex<pioDataset->ntimeranges)) return -1;
 	
-	// extend dataset
-	newExtent[0] = (hsize_t)(pioDataset->stored + dataNumber);
-	ERROR_SWITCH_OFF
-	extend_err = H5Dextend(pioDataset->identifier, newExtent);
-	ERROR_SWITCH_ON
-	if (extend_err < 0) return -1;	
+    if (dataNumber > 0)
+    {
+        // extend dataset
+        newExtent[0] = (hsize_t)(pioDataset->stored + dataNumber);
+        ERROR_SWITCH_OFF
+        extend_err = H5Dextend(pioDataset->identifier, newExtent);
+        ERROR_SWITCH_ON
+        if (extend_err < 0) return -1;	
 	
-	// append data to dataset
-	position[0] = (hsize_t)pioDataset->stored;
-	number[0] = (hsize_t)dataNumber;
-	dataspaceForData = H5Dget_space(pioDataset->identifier);
-	H5Sselect_hyperslab(dataspaceForData, H5S_SELECT_SET, position, NULL, number, NULL);
-	bufferDataspaceForData = H5Screate_simple(1, number, NULL);
-	ERROR_SWITCH_OFF
-	write_err = H5Dwrite(pioDataset->identifier, dataType.identifier, bufferDataspaceForData, dataspaceForData, H5P_DEFAULT, dataBuffer);
-	ERROR_SWITCH_ON
-	H5Sclose(bufferDataspaceForData);
-	H5Sclose(dataspaceForData);
-	if (write_err < 0) return -1;
+        // append data to dataset
+        position[0] = (hsize_t)pioDataset->stored;
+        number[0] = (hsize_t)dataNumber;
+        dataspaceForData = H5Dget_space(pioDataset->identifier);
+        H5Sselect_hyperslab(dataspaceForData, H5S_SELECT_SET, position, NULL, number, NULL);
+        bufferDataspaceForData = H5Screate_simple(1, number, NULL);
+        ERROR_SWITCH_OFF
+        write_err = H5Dwrite(pioDataset->identifier, dataType.identifier, bufferDataspaceForData, dataspaceForData, H5P_DEFAULT, dataBuffer);
+        ERROR_SWITCH_ON
+        H5Sclose(bufferDataspaceForData);
+        H5Sclose(dataspaceForData);
+        if (write_err < 0) return -1;
+    }
 	
 	// update link dataset
 	link.number = dataNumber;
